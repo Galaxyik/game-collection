f6eaf80197d5ef4bccd5127c4d25ef97e8490ebf
Max Schmidt <88938697+Galaxyik@users.noreply.github.com>
2022-06-29 01:01:36 +0200

    feat: Add speakOutput file
    The speakOutput file contains all speakOutputs used in the skill.
    This allows us to managed them from a central location

---
+++ lambda/speakOutputs.js
@@ -0,0 +1,7 @@
+const nameInDB = name => `Hi ${name}`;
+const nameNotInDB = name => `Welcome to Game Collection ${name}! Here you can play many different games.`;
+
+module.exports = {
+    nameInDB,
+    nameNotInDB
+};
