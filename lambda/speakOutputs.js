ad9960d463836ad028cf2cc31e49ab2093b579fe
Max Schmidt <88938697+Galaxyik@users.noreply.github.com>
2022-06-29 18:14:00 +0200

    feat: Update speakOutputs

--- lambda/speakOutputs.js
+++ lambda/speakOutputs.js
@@ -1,11 +1,13 @@
 // userSelection
-const nameInDB = name => `Hi ${name}`;
-const nameNotInDB = name => `Welcome to Game Collection ${name}! Here you can play many different games.`;
+const nameInDB = (name) =>
+    `Hi ${name}. What do you want to play? You can choose from Rock Paper Scissors and Battleships.`;
+const nameNotInDB = (name) =>
+    `Welcome to Game Collection ${name}! Here you can play many different games. You can choose from Rock Paper Scissors and Battleships`;
 
 // gameSelection
-const battleshipsSave = 'Do you want to start a new game, resume from the old save, or get an explanation?';
+const battleshipsSave =
+    'Do you want to start a new game, resume from the old save, or get an explanation?';
 const battleshipsNoSave = 'Do you want to start a new game or get an explanation?';
-
 
 module.exports = {
     nameInDB,
